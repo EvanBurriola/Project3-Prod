e3b9acdb04ba377689679fbcafe757aa309def59
Evan Burriola <evanburriola12@tamu.edu>
2022-11-02 14:15:11 -0500

    FIXED: added Script tag and fixed charSet warning

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
     return (
@@ -7,15 +8,15 @@ export default function Document() {
             {/* bootstrap 5.2 */}
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha256-IUOUHAPazai08QFs7W4MbzTlwEWFo7z/4zw8YmxEiko=" crossOrigin="anonymous" />
             {/* fontawesome reference */}
-            <script src="https://kit.fontawesome.com/b2b606ab07.js" crossOrigin="anonymous"></script>
+            <Script src="https://kit.fontawesome.com/b2b606ab07.js" crossOrigin="anonymous"></Script>
             {/* jquery 3.6.1 */}
-            <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossOrigin="anonymous"></script>
+            <Script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossOrigin="anonymous"></Script>
         </Head>
         <body>
             <Main />
             <NextScript />
             {/* bootstrap 5.2 js */}
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha256-h1OMS35Ij1pJ0S+Y1qBK/GHQDyankPMZVpeZrNQ062U=" crossOrigin="anonymous"></script>
+            <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha256-h1OMS35Ij1pJ0S+Y1qBK/GHQDyankPMZVpeZrNQ062U=" crossOrigin="anonymous"></Script>
         </body>
         </Html>
     )
