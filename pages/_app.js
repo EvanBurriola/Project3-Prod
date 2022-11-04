6beb660f0150e48d4da3380edb0f054d66508f41
Axel Ramone <axel.ramone@gmail.com>
2022-11-04 10:16:09 -0500

    ADDED: redux store provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import '@/styles/main.css'
 import Head from 'next/head'
+import store from '@/store'
+import { Provider } from 'react-redux'
 
 const POSApp = ({ Component, pageProps }) => {
     return (
-        <>
+        <Provider store={store}>
             <Head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <Component {...pageProps} />
-        </>
+        </Provider>
     )
 }
 
