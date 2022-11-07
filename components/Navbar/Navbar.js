adf18cac39f2241b056dec092db7fb7ab5dcf063
Jenna Jung <ymj08130@gmail.com>
2022-11-07 14:18:00 -0600

    Layout, Login View

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
@@ -73,3 +74,27 @@ export const NavbarManager = () => {
         </Navbar>
     )
 }
+=======
+// any imports
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export const ManagerNavbar = () => {
+    return (
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#dashboard">Spin 'N Stone</Navbar.Brand>
+          <Navbar.Toggle aria-controls="manager-navbar" />
+          <Navbar.Collapse id="manager-navbar">
+            <Nav className="navigations">
+              <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+              <Nav.Link href="#inventory">Inventory</Nav.Link>
+              <Nav.Link href="../pages/Manager/employees.js">Employees</Nav.Link>
+              <Nav.Link href="#server">Switch View</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+    );
+}
+>>>>>>> Layout, Login View
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
@@ -65,5 +66,75 @@ export default function Home() {
         </a>
       </footer>
     </div>
+=======
+import { ManagerNavbar } from '@/components/Navbar/Navbar.js';
+
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import DateSelect, { DateEnd, DateStart } from '../components/TextEntry/Datepicker.js';
+import InventoryTable from '@/components/Table/InventoryTable.js';
+import MonthlySales from '@/components/Table/MonthlySales.js';
+import RestockTable from '@/components/Table/RestockTable.js';
+import MenuTable from '@/components/Table/MenuTable.js';
+import { prisma } from '@/lib/prisma'
+
+import styles from "@/styles/manager.module.css"
+
+
+
+export async function getServerSideProps(){
+  const inventory = await prisma.inventory.findMany()
+  const menu = await prisma.menuitems.findMany()
+  return {
+      props: {
+          inventory,
+          menu
+      }
+  }
+}
+
+export default function server({inventory, menu}) {
+  return (
+    <Container>
+      <ManagerNavbar />
+      <Row>
+        <Col> <h1 className = {styles.title}> Dashboard </h1></Col>
+        <p> {"\n"} </p>
+      </Row>
+      <Row>
+        <Col> <h2 className = {styles.header}> Reports </h2> </Col>
+      </Row>
+      <form>
+        <Row>
+          <Col> <DateSelect /> </Col>
+        </Row>
+        <Row> 
+          <Col> 
+            <button className = {styles.button} type = "submit"> Sales </button>
+            <button className = {styles.button} type = "submit"> Excess </button> 
+            <button className = {styles.button} type = "submit"> Restock </button> 
+            <p> {"\n"} </p>
+          </Col>
+        </Row>
+      </form>
+      <Row>
+        <h2 className = {styles.header}> Inventory At a Glance </h2>
+        <InventoryTable inventory={inventory}/>
+      </Row>
+      <Row>
+        <p> {"\n"} </p>
+        <h3 className = {styles.header}> Menu Items </h3>
+        <MenuTable menu={menu}/>
+      </Row>
+      <Row>
+        <p> {"\n"} </p>
+        <h2 className = {styles.header}> Restock Report </h2>
+        <RestockTable inventory={inventory}/>
+      </Row>
+      
+    </Container>
+>>>>>>> Layout, Login View
   )
 }
