310ced97ef2a755458c61895f683829b9ebc02cd
Axel Ramone <axel.ramone@gmail.com>
2022-11-07 11:02:22 -0600

    UPDATED: improved display of order and total using flex util

--- components/Objects/Objects.js
+++ components/Objects/Objects.js
@@ -7,14 +7,6 @@ export const MenuItem = ({butId, name, ...props}) => {
         <>
             <Button className={styles.button} onClick={props.onClick} id={butId} variant="primary" >{name}</Button>
         </>
-<<<<<<< HEAD
-    )
-}
-
-export const Selected = ({...props}) => {
-    const { ingredient } = props
-    return (
-        <p className="fs-5 ps-4 mb-0">- {ingredient.ingredientname}</p>
     )
 }
 
@@ -26,48 +18,24 @@ export const OrderCost = ({...props}) => {
             <p className="fs-5">Subtotal: ${subtotal}</p>
             <p className="fs-5">Tax: ${salestax}</p>
             <p className="fs-3">Total: ${ordertotal}</p>
-=======
+        </div>
     )
 }
 
 export const Selected = ({...props}) => {
     const { ingredient } = props
     return (
-        <p className="fs-5">- {ingredient.ingredientname}</p>
-    )
-}
-
-export const OrderCost = ({...props}) => {
-    const { subtotal, salestax, ordertotal} = props.order
-    
-    return (
-        <div className={styles.orderCostPos}>
-<<<<<<< HEAD
-            <p className="fs-2">Subtotal: ${subTotal}</p>
-            <p className="fs-5">Tax: ${tax}</p>
-            <p className="fs-5">Total: ${total}</p>
->>>>>>> ADDED: button functionality + FIXED: models and view components to better display dynamic buttons
-=======
-            <p className="fs-2">Subtotal: ${subtotal}</p>
-            <p className="fs-5">Tax: ${salestax}</p>
-            <p className="fs-5">Total: ${ordertotal}</p>
->>>>>>> ENHANCE: redux state properties now inline with prisma schema
-        </div>
+        <p className="fs-5 ps-4 mb-0">- {ingredient.ingredientname}</p>
     )
 }
 
 export const OrderDisplay = ({item}) => {
     return (
         <div>
-<<<<<<< HEAD
+
             <p className="fs-3 mb-0">{item.pizzatype}:</p>
             {item.toppings.map(top => {
                 return <Selected key={top.inventoryid} ingredient={top} />
-=======
-            <p className="fs-2">{item.pizzatype}</p>
-            {item.toppings.map(top => {
-                return <Selected ingredient={top} />
->>>>>>> ADDED: button functionality + FIXED: models and view components to better display dynamic buttons
             })
             }
         </div>
