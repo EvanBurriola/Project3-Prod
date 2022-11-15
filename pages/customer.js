f7779b7453df6ada5e91eba263b298079788d319
brandon-longuet <17468+brandon-longuet@users.noreply.github.tamu.edu>
2022-11-15 16:13:58 -0600

    Customer condensing (#5)
    * RESOLVED: CustomerView Condensing
    
    * Removed everything in customer styles
    
    * RESOLVED: CustomerView Condensing
    
    * Removed everything in customer styles
    
    * Revert "Removed everything in customer styles"
    
    This reverts commit 3a0d0dc7bf310d11a9337b4523faf8d281ff3971.
    
    * RESOLVED: CustomerView Condensing
    
    * Removed everything in customer styles
    
    * Revert "Removed everything in customer styles"
    
    This reverts commit 3a0d0dc7bf310d11a9337b4523faf8d281ff3971.
    
    * RESOLVED: CustomerView Condensing
    
    * Removed everything in customer styles
    
    * removed a dependency
    
    * removed a dependency
    
    * RESOLVED: CustomerView Condensing
    
    * Removed everything in customer styles
    
    * Revert "Removed everything in customer styles"
    
    This reverts commit 3a0d0dc7bf310d11a9337b4523faf8d281ff3971.
    
    * RESOLVED: CustomerView Condensing
    
    * Removed everything in customer styles
    
    * removed a dependency
    
    * RESOLVED: CustomerView Condensing
    
    * Revert "Removed everything in customer styles"
    
    This reverts commit 3a0d0dc7bf310d11a9337b4523faf8d281ff3971.
    
    * RESOLVED: CustomerView Condensing
    
    * Removed everything in customer styles
    
    * removed a dependency
    
    * Revert "removed a dependency"
    
    This reverts commit e528371f80891bac1d744d90f6c26822b1b1b023.
    
    * removed a dependency
    
    Co-authored-by: Brandon Longuet <brandon.longuet@yahoo.com>

--- pages/customer.js
+++ pages/customer.js
@@ -1,16 +1,6 @@
-import styles from '@/styles/customer.module.css'
-
-import * as Navbar from "@/components/Navbar/Navbar.js";
-import GridSystem from '@/components/GridSystem/GridSystem.js';
-import * as Object from '@/components/Objects/Objects.js';
-
+import * as View from './CustomerViews/index.js'
 
 import React, { useState, useEffect } from 'react';
-import Container from 'react-bootstrap/Container';
-import Stack from 'react-bootstrap/Stack';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import { prisma } from '@/lib/prisma'
 import { useSelector, useDispatch } from 'react-redux'
@@ -36,11 +26,13 @@ export async function getServerSideProps() {
     }
 }
 
-export default function Cust_start_order({inventory, menu}) {
+export default function Customer({inventory, menu}) {
     const order = useSelector((state) => state.order)
     const dispatch = useDispatch()
 
     const dough = inventory.find(item => item.ingredientname === 'Dough')
+
+    const [custViews, setCustView] = useState([true, false ,false, false])
 
     // activate or deactive checkout button based on order
     const [checkoutReady, setCheckoutReady] = useState(false)
@@ -50,7 +42,7 @@ export default function Cust_start_order({inventory, menu}) {
         } else {
             setCheckoutReady(true)
         }
-    })
+    }, [order.orderItems.length, order.customername])
 
     const handleNewPizza = (type, price) => {
         const pizza = {
@@ -65,6 +57,7 @@ export default function Cust_start_order({inventory, menu}) {
             inventoryid: dough.inventoryid,
             ingredientprice: dough.priceperounce,
             quantityused: dough.averageamountperunitsold,
+            itemtype: dough.itemtype
         }
 
         dispatch(addItem(pizza))
@@ -74,12 +67,17 @@ export default function Cust_start_order({inventory, menu}) {
     // redux function to add toppings to a pizza
     const handleAddTopping = (ingredient) => {
         const lastItem = order.orderItems.length - 1
+        if (!order.orderItems[lastItem]) {
+            return
+        }
+        
         const item = {
             pizzatype: order.orderItems[lastItem].pizzatype,
             ingredientname: ingredient.ingredientname,
             inventoryid: ingredient.inventoryid,
             ingredientprice: ingredient.priceperounce,
             quantityused: ingredient.averageamountperunitsold,
+            itemtype: ingredient.itemtype
         }
         dispatch(addPizzaTopping(item))
     }
@@ -95,33 +93,36 @@ export default function Cust_start_order({inventory, menu}) {
             })
 
             dispatch(clearOrder())
+            setCustView([true,false,false,false]);
         } catch (error) {
             console.log(error);
         }
     }
 
-    return (
-        <div>
-            <Container fluid>
-                <Row>
-                    <Navbar.NavbarCustomer sticky="top"/>
-                </Row>
-                <Row>
-                    <div class={styles.centerScreen1}>
-                        <h1>Start Your Order</h1>
-                    </div>
-                    <div class={styles.centerScreen2}>
-                        <GridSystem colCount={3} md={2} >
-                            {menu.length > 0 ? menu.map(item => {
-                                return <Object.MenuItem key={item.typeid} onClick={() => handleNewPizza(item.pizzatype, item.itemprice)} butId={item.typeid} name={item.pizzatype} />
-                            }) : <p>No tracks are found.</p>
-                            }
-                        </GridSystem>
-                    </div>
-                </Row>
-                <Button style={{position: "absolute", width: "15%", top: "80%", left: "41%"}} href="/Cust_cheese_sauce" variant="primary" >Next Page</Button>
-            </Container>
-        </div>
+    const next_page = (page) => {
+        let temp = [...custViews];
+        temp[page] = false;
+        temp[page + 1] = true;
+        setCustView(temp);
+    }
 
+    const back_page = (page) => {
+        let temp = [...custViews];
+        temp[page] = false;
+        temp[page - 1] = true;
+        setCustView(temp);
+    }
+
+    const add_more = () => {
+        setCustView([true,false,false,false]);
+    }
+
+    return(
+        <>
+            {custViews[0] ? <View.cust_start_order inventory={inventory} menu={menu} order={order} pages={custViews} checkoutReady={checkoutReady} handleNewPizza={handleNewPizza} submitOrder={submitOrder} next_click={next_page}/> : <></>}
+            {custViews[1] ? <View.cust_cheese_sauce inventory={inventory} menu={menu} order={order} pages={custViews} checkoutReady={checkoutReady} handleAddTopping={handleAddTopping} submitOrder={submitOrder} next_click={next_page} back_click={back_page}/> : <></>}
+            {custViews[2] ? <View.cust_toppings inventory={inventory} menu={menu} order={order} pages={custViews} checkoutReady={checkoutReady} handleAddTopping={handleAddTopping} submitOrder={submitOrder} next_click={next_page} back_click={back_page}/> : <></>}
+            {custViews[3] ? <View.cust_drink inventory={inventory} menu={menu} order={order} pages={custViews} checkoutReady={checkoutReady} handleAddTopping={handleAddTopping} add_more={add_more} submitOrder={submitOrder} back_click={back_page} /> : <></>}
+        </>
     );
 }
